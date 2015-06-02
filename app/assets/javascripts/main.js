50c0bd9979cdda6597873e3ffc0e856430b5cd0d
carlos99 <carlos.cw2106@gmail.com>
2015-06-02 12:41:26 -0600

    Applying some styles in order to improve the UX and UI. Now you search form is hide and the index page results. You can get the form again.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $( ".reseach-link" ).click(function(event) {
+      event.preventDefault();
+    $( ".search-again" ).show( "slow", function() {
+  });
+  
+});
+});
